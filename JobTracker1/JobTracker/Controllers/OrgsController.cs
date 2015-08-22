f2d13a74d99a3b67576ab0d555f5a9ac995c83b1
Michael Null <michael.null@bpmw.net>
2015-08-21 22:59:45 -0500

    done for Friday night

---
+++ JobTracker1/JobTracker/Controllers/OrgsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JobTracker.Models;
+
+namespace JobTracker.Controllers
+{
+    public class OrgsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Orgs
+        public ActionResult Index()
+        {
+            return View(db.Orgs.ToList());
+        }
+
+        // GET: Orgs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Org org = db.Orgs.Find(id);
+            if (org == null)
+            {
+                return HttpNotFound();
+            }
+            return View(org);
+        }
+
+        // GET: Orgs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orgs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address1,Address2,City,State,Zip,Website,Email,Phone")] Org org)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orgs.Add(org);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(org);
+        }
+
+        // GET: Orgs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Org org = db.Orgs.Find(id);
+            if (org == null)
+            {
+                return HttpNotFound();
+            }
+            return View(org);
+        }
+
+        // POST: Orgs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address1,Address2,City,State,Zip,Website,Email,Phone")] Org org)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(org).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(org);
+        }
+
+        // GET: Orgs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Org org = db.Orgs.Find(id);
+            if (org == null)
+            {
+                return HttpNotFound();
+            }
+            return View(org);
+        }
+
+        // POST: Orgs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Org org = db.Orgs.Find(id);
+            db.Orgs.Remove(org);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
